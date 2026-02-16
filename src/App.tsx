bb9f351328ae780251cdb6b45fb55db410522235
Ricardo A Serino <ricardoaguadoserino@Ricardos-MacBook-Pro.local>
2026-02-15 23:34:44 -0500

    My task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import "./App.css";
+import "./App.css"
+import {Button} from 'react-bootstrap';
+
+ 
 
 function App(): React.JSX.Element {
     return (
@@ -12,8 +15,19 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-        </div>
+       <h1> <span style={ {color: 'purple'} }>This will be my added header</span></h1>
+       <img src="https://i.redd.it/the-original-marathon-art-by-craig-mullins-v0-ghb3fjqqj22b1.jpg?width=1000&format=pjpg&auto=webp&s=d722042bb899955120a6751e5dc7c8f340da718d" alt="Marathon Concept Art" />    
+       Bungie Games List
+       <ul>
+        <li>Marathon</li>
+        <li>Halo</li>
+        <li>Destiny</li>
+       </ul>
+       
+       <Button onClick={ () => { console.log("Hello World!") } }>Log Hello World</Button>
+    </div>
     );
+
 }
 
 export default App;
