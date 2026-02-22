ebae62e68fd9d96afdd326c3220df3eef4765bd8
Ricardo A Serino <ricardoaguadoserino@Mac.fios-router.home>
2026-02-22 15:37:07 -0500

    finished task 5.1

--- src/arrays.ts
+++ src/arrays.ts
@@ -92,14 +92,8 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    if (addends.length === 0) {
-        return "0=0";
-    }
-
-    const sum = addends.reduce((total, num) => total + num, 0);
-    const expression = addends.join("+");
-
-    return '${sum}=${expression}';
+    if (addends.length === 0) return "0=0";
+    return '${addends.reduce((a,b)=>a+b,0)}=${addends.join("+")}';
 }
 
 /**
